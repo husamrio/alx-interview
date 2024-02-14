c531eea016023d48fbbeef4d718fe0f7676efcdf
husamrio <zulfiqrd@gmail.com>
2024-02-14 17:38:56 +0300

    update

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,35 +1,33 @@
 #!/usr/bin/node
 
+'use strict'; // Add 'use strict' directive
+
 const request = require('request');
 
 const filmNum = process.argv[2] + '/';
 const filmURL = 'https://swapi-api.hbtn.io/api/films/';
 
 /** 
--------------------------------------------- 
-Makes an API request to get film information
-*/ 
-
-
+ * Makes an API request to get film information
+ */ 
 request(filmURL + filmNum, async function (err, res, body) {
   if (err) return console.error(err);
 
   /** 
-   *********************************************************
-   Parse the response body to get the list of character URLs
-  */
+   * Parse the response body to get the list of character URLs
+   */
   const charURLList = JSON.parse(body).characters;
 
-  // Iterare through the character URLs and fect character information
-  // Make a request to each character URL
+  // Iterate through the character URLs and fetch character information
   for (const charURL of charURLList) {
     await new Promise(function (resolve, reject) {
+      // Make a request to each character URL
       request(charURL, function (err, res, body) {
         if (err) return console.error(err);
 
-        // Parse the charcter nformation and print the character's name Resolve the promise to indicate completion
+        // Parse the character information and print the character's name
         console.log(JSON.parse(body).name);
-        resolve();
+        resolve(); // Resolve the promise to indicate completion
       });
     });
   }
--- 0x06-starwars_api/1st_attempt.js
+++ 0x06-starwars_api/1st_attempt.js
@@ -1,29 +1,29 @@
 #!/usr/bin/node
 
+'use strict'; // Add 'use strict' directive
+
 const request = require('request');
 
 const filmNum = process.argv[2] + '/';
 const filmURL = 'https://swapi-api.hbtn.io/api/films/';
 
 /**
- ..............................................
  * Makes an API request to get film Information
- */ 
+ */
 request(filmURL + filmNum, async function (err, res, body) {
   if (err) return console.error(err);
 
-/**
-********************************************************* 
-Parse the response body to get the list of character URLs
-*/
-const charURLList = JSON.parse(body).characters;
+  /**
+   * Parse the response body to get the list of character URLs
+   */
+  const charURLList = JSON.parse(body).characters;
 
-/**
-* Iterate through the character URLs and fetc character information
-*/ 
+  /**
+   * Iterate through the character URLs and fetch character information
+   */
   for (const charURL of charURLList) {
     await new Promise(function (resolve, reject) {
-       // Make a request to each character URl
+      // Make a request to each character URL
 
       request(charURL, function (err, res, body) {
         if (err) return console.error(err);
